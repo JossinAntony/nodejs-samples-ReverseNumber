30504d619c6e5a4fe54d914a5463079f6e7cd395
JossinAntony <jossinde@gmail.com>
2020-10-19 09:36:56 +0200

    write to file implemented.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+const myfs = require('fs');
+
 var num = 999777899;
 var rNum = 0;
+
+var number = num;
 
  while(num != 0){
     rNum = rNum * 10;
     rNum = rNum + (parseInt(num%10));
     num = parseInt(num/10);
   }
+
+  myfs.appendFile('ReversedNo.txt', ('Reverse of ' + number + ' is ' + rNum + '\n'),(e)=>{
+    if(e)throw e;
+  });
   console.log(rNum);
